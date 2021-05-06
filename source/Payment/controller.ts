ba2b231b56138a5736eb080431ef0f4a1a0b5e1a
Godinson1 <josephgodwin274@yahoo.com>
2021-05-06 02:02:25 +0100

    updates

--- source/Payment/controller.ts
+++ source/Payment/controller.ts
@@ -65,7 +65,7 @@ const webhook = async (req: Request, res: Response): Promise<Response> => {
         //--------------------------------------------------------
 
         const isValidIP = checkIp(req.body.data.ip_address)
-        console.log(isValidIP)
+        console.log(req.body)
         if (isValidIP) {
             const chargeResponse = req.body
             userData = await User.findOne({ ref: chargeResponse.data.reference })
