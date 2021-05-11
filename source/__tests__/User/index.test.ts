b40b54eb85513552c0d6f3ef2874307d6156dd59
Godinson1 <josephgodwin274@yahoo.com>
2021-05-11 18:39:44 +0100

    fix auto payment

--- source/__tests__/User/index.test.ts
+++ source/__tests__/User/index.test.ts
@@ -2,9 +2,9 @@ process.env.NODE_ENV = "test"
 import supertest from "supertest"
 import { app } from "../../app"
 import { StatusCodes } from "http-status-codes"
-import { connectToTestDB, closeDBConnection, signInTestUser, success, error } from "../../Utility"
+import { connectToTestDB, closeDBConnection, signInTestUser, success } from "../../Utility"
 
-const { OK, NOT_FOUND } = StatusCodes
+const { OK } = StatusCodes
 const request = supertest(app)
 let token: string
 
@@ -30,17 +30,8 @@ describe("Test for Authentication endpoints", () => {
         expect(data.body.status).toEqual(success)
     }, 10000)
 
-    it("should fail retrieving single user with handle that does not exist", async () => {
-        const data = await request.get("/user/fakehandle").set("mb-token", token)
-        expect(data.status).toEqual(NOT_FOUND)
-        expect(data.body).toHaveProperty("status")
-        expect(data.body).toHaveProperty("message")
-        expect(data.body.message).toEqual("User with @fakehandle does not exist")
-        expect(data.body.status).toEqual(error)
-    }, 10000)
-
-    it("should retrieve single user in database", async () => {
-        const data = await request.get("/user/godinson").set("mb-token", token)
+    it("should retrieve single user details in database", async () => {
+        const data = await request.get("/user/details").set("mb-token", token)
         expect(data.status).toEqual(OK)
         expect(data.body).toHaveProperty("status")
         expect(data.body).toHaveProperty("message")
