85b55f3380cf189621e16efd172fa475c455bf1c
Godinson1 <josephgodwin274@yahoo.com>
2023-01-09 16:57:57 +0100

    update start script

--- dist/error/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorHandler = exports.handleError = void 0;
-const error_1 = require("./error");
-Object.defineProperty(exports, "handleError", { enumerable: true, get: function () { return error_1.handleError; } });
-Object.defineProperty(exports, "ErrorHandler", { enumerable: true, get: function () { return error_1.ErrorHandler; } });
